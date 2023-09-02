3076e0ac9572dcc9a0e985255428f6e159f10631
Mizz <123146044+MrMizzles@users.noreply.github.com>
2023-09-01 22:00:43 -0400

    Integrate API Data, added data validation
    Created a new branch for the myFlix API and temporarily adjusted the /movies endpoint for authentication. In the React app, replaced hardcoded movie data with dynamic data fetched from the myFlix API. Implemented PropTypes for components

--- components/mainview/main-view.jsx
+++ components/mainview/main-view.jsx
@@ -1,29 +1,43 @@
-import { identity } from "lodash";
-import  React  from "react"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import PropTypes from "prop-types";
     
 export const MainView = () => {
-    const [movies, setBooks] = useState([       
-            { id: 1, title: "Raiders of the Lost Ark", year: '1981', description: "A globetrotting archaeologist and college professor vying with Nazi German forces to recover the long-lost Ark of the Covenant which is said to make an army invincible. Teaming up with his tough former romantic interest Marion Ravenwood (Karen Allen), Indiana Jones races to stop rival archaeologist Dr. René Belloq (Paul Freeman) from guiding the Nazis to the Ark and its power.", genre: "Action", image:
-            "https://upload.wikimedia.org/wikipedia/en/a/a6/Raiders_of_the_Lost_Ark_Theatrical_Poster.jpg",
-          director: "Steven Spielberg" },
-            { id: 2, title: "Platoon", year: '1986', description: "The film, based on Stone's experience from the war, follows a U.S. Army volunteer (Sheen) serving in Vietnam while his Platoon Sergeant and his Squad Leader (Berenger and Dafoe) argue over the morality in the platoon and of the war itself.", genre: "War",
-            image:
-              "https://upload.wikimedia.org/wikipedia/en/a/a9/Platoon_posters_86.jpg",
-            director: "Oliver Stone" },
-            { id: 3, title: "Escape From New York", year: "1981", description: "Set in the near-future world of 1997, concerns a crime-ridden United States, which has converted Manhattan Island in New York City into the country's sole maximum security prison. Air Force One is hijacked by anti-government insurgents who deliberately crash it into the walled borough. Ex-soldier and current federal prisoner Snake Plissken (Russell) is given just 24 hours to go in and rescue the President of the United States, after which, if successful, he will be pardoned.",
-            image:
-              "https://upload.wikimedia.org/wikipedia/en/4/4b/EscapefromNYposter.jpg",
-            director: "John Carpenter" }
-    ]);
+    const [movies, setMovies] = useState([]);
+    
+    //creates state changes for selected movies
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
-    const [selectedMovieData, setSelectedMovieData] = useState(null);
+    useEffect(() => {
+        fetch("https://moviesapionrender.onrender.com/movies")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            const moviesFromApi = data.map((movie) => {
+                return {
+                    _id: movie.id,
+                    Title: movie.Title,
+                    ImagePath: movie.ImagePath,
+                    Description: movie.Description,
+                    Genre: {
+                        Name: movie.Genre.Name,
+                        Description: movie.Genre.Description
+                },  
+                    Director: {
+                        Name: movie.Director.Name
+                },
+                    Featured: movie.Featured.toString()
+                   };
+                });
+                
+                setMovies(moviesFromApi);
+            });
+    }, []);
 
-    if (selectedMovieData) {
+    if (selectedMovie) {
         return ( 
-            <MovieView movieData={selectedMovieData} onBackClick={() => setSelectedMovieData(null)} />
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
         );
     }
 
@@ -35,10 +49,10 @@ export const MainView = () => {
         <div>
             {movies.map((movie) => ( 
                 < MovieCard 
-                key={movie.id} 
-                movieData={movie}
-                onMovieClick={(newSelectedMovieData) => {
-                    setSelectedMovieData(newSelectedMovieData);
+                key={movie.Title} 
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                    setSelectedMovie(newSelectedMovie);
                 }}
                 />
             ))}
--- components/movie-view/movie-view.jsx
+++ components/movie-view/movie-view.jsx
@@ -1,25 +1,36 @@
+import PropTypes from "prop-types";
+
+
 export const MovieView = ({ movieData, onBackClick }) => { 
     return ( 
         <div>
             <div>
-                <img src={movieData.image} />
+                <img src={movie.ImagePath} />
             </div>
             <div>
                 <span>Title: </span>
-                <span>{movieData.title}</span>
+                <span>{movie.Title}</span>
             </div>
             <div>
                 <span>Year: </span>
-                <span>{movieData.year}</span>
+                <span>{movie.Year}</span>
+            </div> 
+            <div>
+                <span>Genre: </span>
+                <span>{movie.Genre.Name}</span>
             </div> 
             <div>
                 <span>Description: </span>
-                <span>{movieData.description}</span>
+                <span>{movie.Description}</span>
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movieData.director}</span> 
+                <span>{movie.Director.Name}</span> 
             </div>
+            <div>
+                <span>Featured: </span>
+                <span>{movie.Featured}</span>
+            </div> 
             <div>
                 <span>Genre: </span>
                 <span>{movieData.genre}</span>
@@ -28,3 +39,18 @@ export const MovieView = ({ movieData, onBackClick }) => {
         </div>
     );
 };
+
+MovieView.PropTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        ImagePathh: PropTypes.string.isRequired,
+        Year: PropTypes.string,
+        Description: PropTypes.string,
+           Director: PropTypes.string,
+           Genre: PropTypes.shape({
+             Name: PropTypes.string,
+             Description: PropTypes.string
+        })
+        }).isRequired,
+        onMovieClick: PropTypes.func.isRequired
+    };
